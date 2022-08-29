c61146a3ad92b98b9832bfde20053e3134ca76ff
Muhammad Umair <muhammadumair523@gmail.com>
2022-08-29 16:32:49 +0500

    minor improvemnts

--- routes/appNotifications.js
+++ routes/appNotifications.js
@@ -1,4 +1,4 @@
-import { appId, appToken, baseUrl } from "../constants.js";
+import { appId, appToken, baseUrl, baseUrlIndie } from "../constants.js";
 import axios from "axios";
 import { Router } from "express";
 
@@ -19,7 +19,28 @@ export const sendNotificationToUser = async (
         pushData,
       })
       .then((response) => {
-        console.log(response);
+        // console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const sendNotificationToAll = async (title, body, pushData) => {
+  try {
+    axios
+      .post(baseUrl, {
+        appId,
+        appToken,
+        title,
+        body,
+        pushData,
+      })
+      .then((response) => {
+        // console.log(response);
       })
       .catch((error) => {
         console.log(error);
@@ -34,22 +55,16 @@ const appNotificationsRouter = Router();
 appNotificationsRouter.post("/sendNotificationToAllUsers", (req, res) => {
   const { title, body, pushData } = req.body;
   try {
-    axios
-      .post(baseUrl, {
-        appId,
-        appToken,
-        title,
-        body,
-        pushData,
-      })
-      .then((response) => {
-        res.send("Notification sent successfully");
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    sendNotificationToAll(title, body, pushData);
+    res.send({
+      message: "Notification Sent Successfully",
+    });
   } catch (error) {
     console.log(error);
+    res.send({
+      message: "Notification Failed",
+      error: error,
+    });
   }
 });
 
@@ -57,9 +72,10 @@ appNotificationsRouter.post("/sendNotificationToOneUser", (req, res) => {
   const { userId, title, message, pushData } = req.body;
   try {
     sendNotificationToUser(userId, title, message, pushData);
-    res.send("Notification sent successfully");
+    res.send({ message: "Notification sent successfully" });
   } catch (error) {
     console.log(error);
+    res.send({ message: "Notification failed", error: error });
   }
 });
 
