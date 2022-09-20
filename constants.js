2e00ee7a1cb6e9f9ba08c722a99552b9f78d8a53
Muhammad Umair <muhammadumair523@gmail.com>
2022-09-20 13:44:42 +0500

    new NN account

--- constants.js
+++ constants.js
@@ -1,5 +1,10 @@
-export const appId = 3686;
-export const appToken = "bSmfQdmZN8TAxKjrJdk7Px";
+// export const appId = 3686;
+// export const appToken = "bSmfQdmZN8TAxKjrJdk7Px";
+
+//new account
+export const appId = 3975;
+export const appToken = "KDddBoWdQRdHhgp2D2wR2r";
+
 export const baseUrl = "https://app.nativenotify.com/api/notification";
 export const baseUrlIndie =
   "https://app.nativenotify.com/api/indie/notification";
