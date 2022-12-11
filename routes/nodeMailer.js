d960b3c8edf79a11f65cc2dd966325f5a9d45dba
Muhammad Umair <muhammadumair523@gmail.com>
2022-12-11 19:56:29 +0500

    updated nodemailer

--- routes/nodeMailer.js
+++ routes/nodeMailer.js
@@ -1,15 +1,15 @@
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.mailtrap.io',
-  port: 2525,
-  auth: {
-    user: '304227548d02e2',
-    pass: '629b65871170a9',
-  },
-});
+export const sendEmailToUser = async (email, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: '304227548d02e2',
+      pass: '629b65871170a9',
+    },
+  });
 
-export const sendEmailToUser = (email, subject, text) => {
   const mailOptions = {
     from: 'shareandcare@gmail.com',
     to: email,
@@ -17,11 +17,15 @@ export const sendEmailToUser = (email, subject, text) => {
     text,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
+  await new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        resolve(info);
+      }
+    });
   });
 };
